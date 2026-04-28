dac0e30be0f3f8d744dceb695c1dc025d8fee2d2
tinyhuang <tinyhuang@163.com>
2026-04-28 18:59:30 +0800

    fix(wpf): add missing System.IO import in App startup logger

--- src/D2RMultiPlay.Wpf/App.xaml.cs
+++ src/D2RMultiPlay.Wpf/App.xaml.cs
@@ -4,6 +4,7 @@
 // ============================================================
 
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Security.Principal;
 using System.Threading;
