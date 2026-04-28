e7f8a8fc6ae431657c923c66286a1478a5d9a849
tinyhuang <tinyhuang@163.com>
2026-04-28 19:22:09 +0800

    fix(wpf): add missing System.Windows.Controls imports

--- src/D2RMultiPlay.Wpf/AccountEditDialog.xaml.cs
+++ src/D2RMultiPlay.Wpf/AccountEditDialog.xaml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Windows;
+using System.Windows.Controls;
 using Microsoft.Win32;
 using D2RMultiPlay.Core.Config;
 
--- src/D2RMultiPlay.Wpf/MainWindow.xaml.cs
+++ src/D2RMultiPlay.Wpf/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Media;
 using Microsoft.Win32;
